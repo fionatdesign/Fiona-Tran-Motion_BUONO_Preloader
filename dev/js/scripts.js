d3e6abb7ccc86fb22fefc396a0340d6ac34747ee
fionatdesign <ft98@drexel.edu>
2022-05-05 19:43:14 -0400

    import gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,11 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
+// MorphSVGPlugin.convertToPath("#Circle");
+
+const mainTL = gsap.timeline({id: "mainTL"});
