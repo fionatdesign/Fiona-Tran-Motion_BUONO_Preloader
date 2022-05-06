62bd2e1a877a42c5a2bae9787e507cc9e21d7a05
fionatdesign <ft98@drexel.edu>
2022-05-05 23:59:30 -0400

    added animation to pasta

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,6 +6,54 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(GSDevTools);
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
-// MorphSVGPlugin.convertToPath("#Circle");
+MorphSVGPlugin.convertToPath("#circle");
 
 const mainTL = gsap.timeline({id: "mainTL"});
+
+function orangeCircle(){
+    let tl = gsap.timeline();
+
+    tl.from("#circle", {duration:2, scale: 0.5, transformOrigin:"center", ease: "elastic.out(1, 0.3)"})
+    ;
+
+    return tl;
+    
+}
+
+function pastaShape(){
+    let tl = gsap.timeline();
+
+    tl.to("#outline", {visibility: "visible"})
+    .from("#outline", {duration:1, transformOrigin:"center", drawSVG: "50% 50%"})
+    .from("#outline", {duration:1, fill: "#E37A30"})
+    .to("#outline", {duration:1, fill:"#E3CEB0"})
+
+    return tl;
+    
+}
+
+function pastaOutline(){
+    let tl = gsap.timeline();
+
+    tl.from("#Vector", {duration:1, drawSVG: 0}, "outline")
+    .from("#Vector_2", {duration:1, drawSVG: 0}, "outline")
+    .from("#Vector_3", {duration:1, drawSVG: 0}, "outline")
+    .from("#Vector_4", {duration:1, drawSVG: 0}, "outline")
+    ;
+
+    return tl;
+    
+}
+
+// function pastaSticker (){
+//     let tl = gsap.timeline();
+
+//     tl.to("#pasta", {duration: 2, rotate: 360})
+// }
+
+mainTL.add(orangeCircle(), "animate")
+.add(pastaShape(), "animate+=0.75")
+.add(pastaOutline(),"animate+=0.85")
+.to("#pasta", {transformOrigin: "center", duration: 1.5, ease: "power3.out", rotation: 180, repeat: 3})
+// .add(pastaSticker(),"animate+=1")
+;
