c12a76bba2c9fbdc2de836a13ff133b7a5829f7a
fionatdesign <ft98@drexel.edu>
2022-05-06 11:39:03 -0400

    added loading animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,7 +6,7 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(GSDevTools);
 gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
-MorphSVGPlugin.convertToPath("#circle");
+MorphSVGPlugin.convertToPath("#circle, .dot");
 
 const mainTL = gsap.timeline({id: "mainTL"});
 
@@ -45,14 +45,29 @@ function pastaOutline(){
     
 }
 
+function loadingSign(){
+    let tl = gsap.timeline();
+
+    tl.to("#L", {visibility: "visible", delay: 3.5}, "loading")
+    tl.to("#O", {visibility: "visible", delay: 3.7}, "loading")
+    tl.to("#A", {visibility: "visible", delay: 3.9}, "loading")
+    tl.to("#D", {visibility: "visible", delay: 4.1}, "loading")
+    tl.to("#I", {visibility: "visible", delay: 4.3}, "loading")
+    tl.to("#N", {visibility: "visible", delay: 4.5}, "loading")
+    tl.to("#G", {visibility: "visible", delay: 4.7}, "loading")
+    tl.to(".dot", {visibility: "visible", stagger: { each: 0.3 }, duration: 0.3, delay: 4.9, repeat: 3}, "loading")
+    // tl.to("#dot1", {visibility: "visible", delay: 4.9}, "loading")
+    // tl.to("#dot2", {visibility: "visible", delay: 5.1}, "loading")
+    // tl.to("#dot3", {visibility: "visible", delay: 5.3}, "loading")
+}
+
 
 mainTL.add(orangeCircle(), "animate")
 .add(pastaShape(), "animate+=0.75")
 .add(pastaOutline({visibility: "visible"}),"animate+=0.85")
 .to("#details", {visibility: "visible"}, "animate+=0.84")
 .to("#pasta", {transformOrigin: "center", duration: 1.25, ease: "power3.out", rotation: 180, repeat: 3},"animate+=3.5")
-
-
+.add(loadingSign())
 
 
 ;
