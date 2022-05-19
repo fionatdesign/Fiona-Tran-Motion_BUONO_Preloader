c5e12552c7fbcbeb898b64f4fbd7dab8472dccdf
fionatdesign <ft98@drexel.edu>
2022-05-19 16:25:54 -0400

    adjust timing of loading dots and spin

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -24,10 +24,9 @@ function pastaShape(){
     let tl = gsap.timeline();
 
     tl.to("#outline", {visibility: "visible"})
-    .fromTo("#outline", {duration:1, transformOrigin:"center", drawSVG: "10% 10%"},{duration: 1, drawSVG: "0% 100%"})
-    .from("#outline", {duration:1, fill: "#E37A30"})
+    .fromTo("#outline", {duration:.75, transformOrigin:"center", drawSVG: "10% 10%"},{duration: 1, drawSVG: "0% 100%"})
+    .from("#outline", {duration:.75, fill: "#E37A30"})
     .to("#outline", {duration:1, fill:"#E3CEB0"})
-
     return tl;
     
 }
@@ -39,6 +38,9 @@ function pastaOutline(){
     .from("#Vector_2", {duration:.75, drawSVG: 0}, "outline")
     .from("#Vector_3", {duration:.75, drawSVG: 0}, "outline")
     .from("#Vector_4", {duration:.75, drawSVG: 0}, "outline")
+    .to("#pasta", {transformOrigin: "center", duration: 1.5, ease: "power3.out", delay: 4.7, rotation: 180, repeat: -1})
+    // .from("#pasta", {transformOrigin: "center", duration: 1.35, ease: "power4.out", delay: 4.68, rotation: -180, repeat: -1})
+    //^ to sync up loading and spin
     ;
 
     return tl;
@@ -48,20 +50,25 @@ function pastaOutline(){
 function loadingSign(){
     let tl = gsap.timeline();
 
-    tl.to(".loading", {visibility: "hidden", duration: 2})
-    tl.from(".loading", {y:50, skewX:30, ease: "elastic.out (2, 0.5)", visibility: "visible", stagger: {each: 0.3}})
-    tl.to(".loading", {delay: 3.5, visibility: "visible", stagger: {each: 0.3}})
+    tl.fromTo(".loading", {visibility: "visible", duration: 1}, {visibility: "hidden", duration: .5})
+    .from(".loading", {y:50, skewX:50, ease: "elastic.out (2, 0.5)", visibility: "visible", stagger: {each: 0.15}})
+    .to(".loading", {delay: 0, visibility: "visible", stagger: {each: 0.3}})
+    .to(".dot", {visibility: "hidden"})
+    .to(".dot2", {delay: 0.09, visibility: "visible", stagger: { each: 0.440 }, repeat: -1})
 
 }
 
 
-mainTL.add(orangeCircle(), "animate")
+mainTL
+.add(orangeCircle(), "animate")
 .add(pastaShape(), "animate+=0.75")
 .add(pastaOutline({visibility: "visible"}),"animate+=0.85")
-.to("#details", {visibility: "visible"}, "animate+=0.84")
-.to("#pasta", {transformOrigin: "center", duration: 1.25, ease: "power3.out", rotation: 180, repeat: -1},"animate+=4")
-.add(loadingSign())
-.to(".dot", {visibility: "hidden"})
-.to(".dot2", {visibility: "visible", stagger: { each: 0.4 }, delay: 0, repeat: -1})
+.add(loadingSign(),"animate+=0.75")
+.to("#details", {visibility: "visible"}, "animate+=0.85")
+
+// .to("#pasta", {transformOrigin: "center", duration: 1.25, ease: "power3.out", delay: 0, rotation: 180, repeat: -1},"animate+=6.5")
+// .to(".dot", {visibility: "hidden"})
+// .to(".dot2", {delay: 0, visibility: "visible", stagger: { each: 0.3 }, repeat: -1})
+// .to(".dot2", {visibility: "visible", stagger: { each: 0.4 }, delay:0, repeat: -1})
 
 ;
